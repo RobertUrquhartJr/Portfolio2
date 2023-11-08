a7c0354a4473dc76e1a887dccd7c5db3ad5455ed
ROBERT URQUHART JR <ROBERT.URQUHARTJR@GMAIL.COM>
2023-11-08 17:28:26 -0500

    added modal and temp styling

--- script.js
+++ script.js
@@ -90,3 +90,22 @@ window.addEventListener('scroll', function () {
     }
 });
 
+const modal = document.getElementById("contactModal");
+
+const btn = document.getElementById("contactBtn");
+
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
